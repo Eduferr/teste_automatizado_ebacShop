81aefcfc2176595a15bfd11c79f35ce8fc261d4e
Eduferr <eduferr2803@gmail.com>
2025-10-03 00:51:50 -0300

    Cria testes automatizados Carrinho de Compras

---
+++ cypress/e2e/carrinhoCompras/comprasTest.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+const comprasActions = require('../../support/actions/comprasActions');
+const ComprasActions = require('../../support/actions/comprasActions');
+const loginactions = require('../../support/actions/loginactions');
+
+describe('Suíte - Carrinho de Compras', () => {
+
+    beforeEach(() => {
+        cy.visit('/produtos'); // Visita a página inicial antes de cada teste
+    });
+
+    it.only('Deve efetuar a compra buscando pelo nome do produto', () => {
+        cy.fixture('produtos').then((produtos) => {// Importa os dados do arquivo JSON
+            const produto = produtos[0];
+
+        comprasActions.buscarProduto(produto.nome);
+        comprasActions.selecionarCaracteristica(produto.tamanho, produto.cor, produto.quantidade);
+        comprasActions.AdicionarAoCarrinho();
+        comprasActions.validarMensagemDeSucesso(`${produto.quantidade} × “${produto.nome}” foram adicionados no seu carrinho.`);
+        comprasActions.verCarrinho();
+        comprasActions.concluirCompra();
+        loginactions.loginComCredenciaisValidas();
+        comprasActions.pagarCompra();
+        comprasActions.validarMensagemDeSucessoOrder('Obrigado. Seu pedido foi recebido.');
+        });
+    });
+
+    
+
+});
--- cypress/support/page_objects/CadastroPage.js
+++ cypress/support/page_objects/CadastroPage.js
@@ -23,3 +23,4 @@ class CadastroPage {
 }
 
 module.exports = new CadastroPage();
+
